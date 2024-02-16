b727cc32de4554041ad638b75e32038b8e616699
markrizkalla <mark.rizkalla1@gmail.com>
2024-02-16 21:28:14 +0200

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    return "Hello HBNB!"
 
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
